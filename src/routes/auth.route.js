e2ad2c6e1bd23ada80048ad62c5a3511a9a0fdb6
vipin760 <vipinm500@gmail.com>
2026-03-28 16:29:10 +0530

    authetication added

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import usermodel from "../models/auth.model.js";
 import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
 
 export default async function authRoutes(fastify) {
 //  fastify.get("/",async(req,res)=>{
@@ -25,14 +26,19 @@ export default async function authRoutes(fastify) {
     const { username, password } = req.body
      try {
         const userData = await usermodel.findOne({username});
+        console.log("<><>userData",userData);
+        
         if(!userData){
             return reply.code(200).send({status:false,statuscode:403,message:"incorrect user name"})
         }
         const passwordMatch = await bcrypt.compare(password,userData.password);
+        
         if(!passwordMatch){
              return reply.code(200).send({status:false,statuscode:403,message:"incorrect password"})
         }
-        reply.code(200).send({status:true,userData})
+        const {fullname,role} = userData
+        const token = jwt.sign({id:userData._id,username,role},process.env.JWT_SECRET,{expiresIn:'30d'});
+        reply.code(200).send({status:true,user:{username:userData.username,fullname,role},token})
      } catch (error) {
         return reply.code(500).send(error.message)
      }
